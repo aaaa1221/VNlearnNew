f5156cddc994c24f2c178b21000300cb8976d089
vancuong <vancuong@vancuong-PC>
2018-12-10 17:10:31 +0700

    bai1 C -> F

---
+++ Vnlearn/src/package1/Bai1.java
@@ -0,0 +1,44 @@
+package package1;
+
+import java.util.Scanner;
+
+public class Bai1 {
+	/**
+	 * Create by: vancuong - CTO VnLearn
+	 * Create date: 10 thg 12, 2018
+	 * Modifier: vancuong
+	 * Modified date: 10 thg 12, 2018
+	 * Description: ....
+	 * Version 1.0
+	 * @param celsius
+	 * @return
+	 */
+	static double fah(double celsius) {
+		return (double) 9 / 5 * celsius + 32;
+	}
+
+	/**
+	 * Create by: vancuong - CTO VnLearn
+	 * Create date: 10 thg 12, 2018
+	 * Modifier: vancuong
+	 * Modified date: 10 thg 12, 2018
+	 * Description: ....
+	 * Version 1.0
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		 double celsius; 
+		    Scanner scanner = new Scanner(System.in);
+		         
+		    System.out.println("Nhập vào độ Celcius cần đổi: ");
+		    celsius = scanner.nextDouble();
+		 
+		   
+		    System.out.println(celsius + " độ C = " + fah(celsius) + " độ F");
+		
+		
+	}
+	
+	
+
+}
